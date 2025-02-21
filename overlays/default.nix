31c2876dcdb1d28156c532cb8b1ab01b4ca6929c
empunkt <102794+nevesenin@users.noreply.github.com>
2025-02-21 11:51:12 +0100

    Ignore errors

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,14 +1,13 @@
-{ u-boot-src
-, rpi-linux-stable-src
-, rpi-linux-6_6_67-src
-, rpi-linux-6_12_11-src
-, rpi-firmware-src
-, rpi-firmware-nonfree-src
-, rpi-bluez-firmware-src
-, ...
-}:
-final: prev:
-let
+{
+  u-boot-src,
+  rpi-linux-stable-src,
+  rpi-linux-6_6_67-src,
+  rpi-linux-6_12_11-src,
+  rpi-firmware-src,
+  rpi-firmware-nonfree-src,
+  rpi-bluez-firmware-src,
+  ...
+}: final: prev: let
   versions = {
     v6_6_51.src = rpi-linux-stable-src;
     v6_6_67.src = rpi-linux-6_6_67-src;
@@ -25,16 +24,18 @@ let
       ];
     };
   };
-  boards = [ "bcm2711" "bcm2712" ];
+  boards = ["bcm2711" "bcm2712"];
 
   # Helpers for building the `pkgs.rpi-kernels' map.
-  rpi-kernel = { version, board }:
-    let
-      kernel = builtins.getAttr version versions;
-      version-slug = builtins.replaceStrings [ "v" "_" ] [ "" "." ] version;
-    in
-    {
-      "${version}"."${board}" = (final.buildLinux {
+  rpi-kernel = {
+    version,
+    board,
+  }: let
+    kernel = builtins.getAttr version versions;
+    version-slug = builtins.replaceStrings ["v" "_"] ["" "."] version;
+  in {
+    "${version}"."${board}" =
+      (final.buildLinux {
         modDirVersion = version-slug;
         version = version-slug;
         pname = "linux-rpi";
@@ -62,65 +63,74 @@ let
         };
         features.efiBootStub = false;
         kernelPatches =
-          if kernel ? "patches" then kernel.patches else [ ];
-      }).overrideAttrs
-        (oldAttrs: {
-          postConfigure = ''
-            # The v7 defconfig has this set to '-v7' which screws up our modDirVersion.
-            sed -i $buildRoot/.config -e 's/^CONFIG_LOCALVERSION=.*/CONFIG_LOCALVERSION=""/'
-            sed -i $buildRoot/include/config/auto.conf -e 's/^CONFIG_LOCALVERSION=.*/CONFIG_LOCALVERSION=""/'
-          '';
-        });
-    };
-  rpi-kernels = builtins.foldl'
-    (b: a: final.lib.recursiveUpdate b (rpi-kernel a))
-    { };
-in
-{
-  # disable firmware compression so that brcm firmware can be found at
-  # the path expected by raspberry pi firmware/device tree
-  compressFirmwareXz = x: x;
-  compressFirmwareZstd = x: x;
-
-  # provide generic rpi arm64 u-boot
-  uboot-rpi-arm64 = final.buildUBoot rec {
-    defconfig = "rpi_arm64_defconfig";
-    extraMeta.platforms = [ "aarch64-linux" ];
-    filesToInstall = [ "u-boot.bin" ];
-    version = "2024.04";
-    patches = [ ];
-    makeFlags = [ ];
-    src = u-boot-src;
-    # In raspberry pi sbcs the firmware manipulates the device tree in
-    # a variety of ways before handing it off to the linux kernel. [1]
-    # Since we have installed u-boot in place of a linux kernel we may
-    # pass the device tree passed by the firmware onto the kernel, or
-    # we may provide the kernel with a device tree of our own. This
-    # configuration uses the device tree provided by firmware so that
-    # we don't have to be aware of all manipulation done by the
-    # firmware and attempt to mimic it.
-    #
-    # 1. https://forums.raspberrypi.com/viewtopic.php?t=329799#p1974233
+          if kernel ? "patches"
+          then kernel.patches
+          else [];
+        ignoreConfigErrors = true;
+      })
+      .overrideAttrs
+      (oldAttrs: {
+        postConfigure = ''
+          # The v7 defconfig has this set to '-v7' which screws up our modDirVersion.
+          sed -i $buildRoot/.config -e 's/^CONFIG_LOCALVERSION=.*/CONFIG_LOCALVERSION=""/'
+          sed -i $buildRoot/include/config/auto.conf -e 's/^CONFIG_LOCALVERSION=.*/CONFIG_LOCALVERSION=""/'
+        '';
+      });
   };
+  rpi-kernels =
+    builtins.foldl'
+    (b: a: final.lib.recursiveUpdate b (rpi-kernel a))
+    {};
+in
+  {
+    # disable firmware compression so that brcm firmware can be found at
+    # the path expected by raspberry pi firmware/device tree
+    compressFirmwareXz = x: x;
+    compressFirmwareZstd = x: x;
 
-  # default to latest firmware
-  raspberrypiWirelessFirmware = final.callPackage
-    (
-      import ./raspberrypi-wireless-firmware.nix {
-        bluez-firmware = rpi-bluez-firmware-src;
-        firmware-nonfree = rpi-firmware-nonfree-src;
+    # provide generic rpi arm64 u-boot
+    uboot-rpi-arm64 = final.buildUBoot rec {
+      defconfig = "rpi_arm64_defconfig";
+      extraMeta.platforms = ["aarch64-linux"];
+      filesToInstall = ["u-boot.bin"];
+      version = "2024.04";
+      patches = [];
+      makeFlags = [];
+      src = u-boot-src;
+      # In raspberry pi sbcs the firmware manipulates the device tree in
+      # a variety of ways before handing it off to the linux kernel. [1]
+      # Since we have installed u-boot in place of a linux kernel we may
+      # pass the device tree passed by the firmware onto the kernel, or
+      # we may provide the kernel with a device tree of our own. This
+      # configuration uses the device tree provided by firmware so that
+      # we don't have to be aware of all manipulation done by the
+      # firmware and attempt to mimic it.
+      #
+      # 1. https://forums.raspberrypi.com/viewtopic.php?t=329799#p1974233
+    };
+
+    # default to latest firmware
+    raspberrypiWirelessFirmware =
+      final.callPackage
+      (
+        import ./raspberrypi-wireless-firmware.nix {
+          bluez-firmware = rpi-bluez-firmware-src;
+          firmware-nonfree = rpi-firmware-nonfree-src;
+        }
+      )
+      {};
+    raspberrypifw = prev.raspberrypifw.overrideAttrs (oldfw: {src = rpi-firmware-src;});
+  }
+  // {
+    # rpi kernels and firmware are available at
+    # `pkgs.rpi-kernels.<VERSION>.<BOARD>'.
+    #
+    # For example: `pkgs.rpi-kernels.v6_6_67.bcm2712'
+    rpi-kernels = rpi-kernels (
+      final.lib.cartesianProduct
+      {
+        board = boards;
+        version = builtins.attrNames versions;
       }
-    )
-    { };
-  raspberrypifw = prev.raspberrypifw.overrideAttrs (oldfw: { src = rpi-firmware-src; });
-
-} // {
-  # rpi kernels and firmware are available at
-  # `pkgs.rpi-kernels.<VERSION>.<BOARD>'. 
-  #
-  # For example: `pkgs.rpi-kernels.v6_6_67.bcm2712'
-  rpi-kernels = rpi-kernels (
-    final.lib.cartesianProduct
-      { board = boards; version = (builtins.attrNames versions); }
-  );
-}
+    );
+  }
